2e1c924e47d4ea3a06fe6f7c9778bf0548014eed
Soumitra Pandit <smtrpndt@gmail.com>
2022-11-14 02:18:07 -0500

    Added the plotting function. Also added a fucntion to examine the ratio of successive calculations. Had fun.

--- lucas_number_growth.py
+++ lucas_number_growth.py
@@ -1,4 +1,5 @@
 import time
+import matplotlib.pyplot as plt
 
 def lucas(n):
     if n==0:
@@ -9,10 +10,23 @@ def lucas(n):
         return lucas(n-1)+lucas(n-2)
 
 def main():
+    time_list = []
+    x_axis=[]
     for i in range (40):
         delta_time = get_lucas_time(i)
+        time_list.append(delta_time)
+        x_axis.append(i)
         print("n = {a}, time = {b}".format(a=i,b=delta_time))
-    
+    ratio=[]
+    range
+    for j in range(21,(len(time_list)-1)):
+        ratio.append(time_list[j]/time_list[j-1])
+
+    print(ratio)
+
+    plt.plot(x_axis,time_list)
+    plt.show()
+
 def get_lucas_time(n):
     start=time.time()
     lucas(n)
